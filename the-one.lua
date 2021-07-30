5139a0cff94fd369a5987252195716e6903bbf90
r1ck3h <86393693+r1ck3h@users.noreply.github.com>
2021-07-30 17:50:53 +0800

    Update the-one.lua

--- the-one.lua
+++ the-one.lua
@@ -1008,18 +1008,31 @@ MiscellaneousTabCategoryMain:AddButton("Inject Custom Skins", function()
 	end
 end)
 
-MiscellaneousTabCategoryMain:AddButton("Crash Server", function()
-	if LocalPlayer.Character then
-		game:GetService("RunService").RenderStepped:Connect(function()
-			game:GetService("ReplicatedStorage").Events.ThrowGrenade:FireServer(game:GetService("ReplicatedStorage").Weapons["Molotov"].Model, nil, 25, 35, Vector3.new(0,-100,0), nil, nil)
-			game:GetService("ReplicatedStorage").Events.ThrowGrenade:FireServer(game:GetService("ReplicatedStorage").Weapons["HE Grenade"].Model, nil, 25, 35, Vector3.new(0,-100,0), nil, nil)
-			game:GetService("ReplicatedStorage").Events.ThrowGrenade:FireServer(game:GetService("ReplicatedStorage").Weapons["Decoy Grenade"].Model, nil, 25, 35, Vector3.new(0,-100,0), nil, nil)
-			game:GetService("ReplicatedStorage").Events.ThrowGrenade:FireServer(game:GetService("ReplicatedStorage").Weapons["Smoke Grenade"].Model, nil, 25, 35, Vector3.new(0,-100,0), nil, nil)
-			game:GetService("ReplicatedStorage").Events.ThrowGrenade:FireServer(game:GetService("ReplicatedStorage").Weapons["Flashbang"].Model, nil, 25, 35, Vector3.new(0,-100,0), nil, nil)
+MiscellaneousTabCategoryMain:AddButton("Crash Server(its just kill all lol)", function()
+	KillAllLoop = game:GetService("RunService").RenderStepped:Connect(function()
+		wait()
+		pcall(function()
+			for i,v in pairs(game.Players:GetChildren()) do
+				if v ~= LocalPlayer and IsAlive(v) and IsAlive(LocalPlayer) then
+					local Arguments = {
+						[1] = v.Character.Head,
+						[2] = v.Character.Head.Position,
+						[3] = "Banana",
+						[4] = 100,
+						[5] = LocalPlayer.Character.Gun,
+						[8] = 100,
+						[9] = false,
+						[10] = false,
+						[11] = Vector3.new(),
+						[12] = 100,
+						[13] = Vector3.new()
+					}
+					game.ReplicatedStorage.Events.HitPart:FireServer(unpack(Arguments))
+				end
+			end
 		end)
-	end
+	end)
 end)
-
 
 MiscellaneousTabCategoryMain:AddButton("Arsenal Emotes", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/r1ck3h/hexagon/main/arsenal%20emotes"))()
@@ -1034,6 +1047,7 @@ MiscellaneousTabCategoryMain:AddToggle("Anti Vote Kick", false, "MiscellaneousTa
 MiscellaneousTabCategoryMain:AddToggle("Unlock Reset Character", false, "MiscellaneousTabCategoryMainUnlockResetCharacter", function(val)
 	game:GetService("StarterGui"):SetCore("ResetButtonCallback", val)
 end)
+
 
 MiscellaneousTabCategoryMain:AddToggle("Unlock Shop While Alive", false, "MiscellaneousTabCategoryMainUnlockShopWhileAlive")
 
