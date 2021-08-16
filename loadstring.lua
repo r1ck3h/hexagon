d29ae750f61d92bf97be63317bbbde089aa59d1e
r1ck3h <86393693+r1ck3h@users.noreply.github.com>
2021-08-16 11:58:48 +0800

    Update and rename the-one-loadstring.lua to loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/r1ck3h/hexagon/main/script.lua"))()
--- the-one-loadstring.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/r1ck3h/hexagon/main/the-one.lua"))()
